3c28e2f8e2622d353b30f3ae0463e51554060b72
Daniel Maciel Acevedo <anndannma@outlook.com>
2023-03-27 07:25:23 -0600

    Agregué funcionalidad al calendario

---
+++ js/calendario-20.js
@@ -0,0 +1,284 @@
+const maestro1 = document.querySelector('.maestro1');
+const dataMaestro1 = document.querySelector('.table-data1');
+let verifyMaestro1 = false;
+
+
+maestro1.addEventListener('click', () => {
+    if(!verifyMaestro1) {
+        dataMaestro1.classList.add('on');
+        verifyMaestro1 = true;
+    } else {
+        dataMaestro1.classList.remove('on')
+        verifyMaestro1 = false;
+    }
+});
+
+const maestro2 = document.querySelector('.maestro2');
+const dataMaestro2 = document.querySelector('.table-data2');
+let verifyMaestro2 = false;
+
+maestro2.addEventListener('click', () => {
+    if(!verifyMaestro2) {
+        dataMaestro2.classList.add('on');
+        verifyMaestro2 = true;
+    } else {
+        dataMaestro2.classList.remove('on');
+        verifyMaestro2 = false;
+    }
+})
+
+const maestro3 = document.querySelector('.maestro3');
+const dataMaestro3 = document.querySelector('.table-data3');
+let verifyMaestro3 = false;
+
+maestro3.addEventListener('click' , () => {
+    if(!verifyMaestro3) {
+        dataMaestro3.classList.add('on');
+        verifyMaestro3 = true;
+    } else {
+        dataMaestro3.classList.remove('on');
+        verifyMaestro3 = false;
+    }
+})
+
+const maestro4 = document.querySelector('.maestro4');
+const dataMaestro4 = document.querySelector('.table-data4');
+let verifyMaestro4 = false;
+
+maestro4.addEventListener('click', () => {
+    if(!verifyMaestro4) {
+        dataMaestro4.classList.add('on');
+        verifyMaestro4 = true;
+    } else {
+        dataMaestro4.classList.remove('on');
+        verifyMaestro4 = false;
+    }
+})
+
+const maestro5 = document.querySelector('.maestro5');
+const dataMaestro5 = document.querySelector('.table-data5');
+let verifyMaestro5 = false;
+
+
+maestro5.addEventListener('click', () => {
+    if(!verifyMaestro5) {
+        dataMaestro5.classList.add('on');
+        verifyMaestro5 = true;
+    } else {
+        dataMaestro5.classList.remove('on')
+        verifyMaestro5 = false;
+    }
+});
+
+const maestro6 = document.querySelector('.maestro6');
+const dataMaestro6 = document.querySelector('.table-data6');
+let verifyMaestro6 = false;
+
+maestro6.addEventListener('click', () => {
+    if(!verifyMaestro6) {
+        dataMaestro6.classList.add('on');
+        verifyMaestro6 = true;
+    } else {
+        dataMaestro6.classList.remove('on');
+        verifyMaestro6 = false;
+    }
+})
+
+const maestro7 = document.querySelector('.maestro7');
+const dataMaestro7 = document.querySelector('.table-data7');
+let verifyMaestro7 = false;
+
+maestro7.addEventListener('click' , () => {
+    if(!verifyMaestro7) {
+        dataMaestro7.classList.add('on');
+        verifyMaestro7 = true;
+    } else {
+        dataMaestro7.classList.remove('on');
+        verifyMaestro7 = false;
+    }
+})
+
+const maestro8 = document.querySelector('.maestro8');
+const dataMaestro8 = document.querySelector('.table-data8');
+let verifyMaestro8 = false;
+
+maestro8.addEventListener('click', () => {
+    if(!verifyMaestro8) {
+        dataMaestro8.classList.add('on');
+        verifyMaestro8 = true;
+    } else {
+        dataMaestro8.classList.remove('on');
+        verifyMaestro8 = false;
+    }
+})
+
+const maestro9 = document.querySelector('.maestro9');
+const dataMaestro9 = document.querySelector('.table-data9');
+let verifyMaestro9 = false;
+
+
+maestro9.addEventListener('click', () => {
+    if(!verifyMaestro9) {
+        dataMaestro9.classList.add('on');
+        verifyMaestro9 = true;
+    } else {
+        dataMaestro9.classList.remove('on')
+        verifyMaestro9 = false;
+    }
+});
+
+const maestro10 = document.querySelector('.maestro10');
+const dataMaestro10 = document.querySelector('.table-data10');
+let verifyMaestro10 = false;
+
+maestro10.addEventListener('click', () => {
+    if(!verifyMaestro10) {
+        dataMaestro10.classList.add('on');
+        verifyMaestro10 = true;
+    } else {
+        dataMaestro10.classList.remove('on');
+        verifyMaestro10 = false;
+    }
+})
+
+const maestro11 = document.querySelector('.maestro11');
+const dataMaestro11 = document.querySelector('.table-data11');
+let verifyMaestro11 = false;
+
+maestro11.addEventListener('click' , () => {
+    if(!verifyMaestro11) {
+        dataMaestro11.classList.add('on');
+        verifyMaestro11 = true;
+    } else {
+        dataMaestro11.classList.remove('on');
+        verifyMaestro11 = false;
+    }
+})
+
+const maestro12 = document.querySelector('.maestro12');
+const dataMaestro12 = document.querySelector('.table-data12');
+let verifyMaestro12 = false;
+
+maestro12.addEventListener('click', () => {
+    if(!verifyMaestro12) {
+        dataMaestro12.classList.add('on');
+        verifyMaestro12 = true;
+    } else {
+        dataMaestro12.classList.remove('on');
+        verifyMaestro12 = false;
+    }
+})
+
+const maestro13 = document.querySelector('.maestro13');
+const dataMaestro13 = document.querySelector('.table-data13');
+let verifyMaestro13 = false;
+
+
+maestro13.addEventListener('click', () => {
+    if(!verifyMaestro13) {
+        dataMaestro13.classList.add('on');
+        verifyMaestro13 = true;
+    } else {
+        dataMaestro13.classList.remove('on')
+        verifyMaestro13 = false;
+    }
+});
+
+const maestro14 = document.querySelector('.maestro14');
+const dataMaestro14 = document.querySelector('.table-data14');
+let verifyMaestro14 = false;
+
+maestro14.addEventListener('click', () => {
+    if(!verifyMaestro14) {
+        dataMaestro14.classList.add('on');
+        verifyMaestro14 = true;
+    } else {
+        dataMaestro14.classList.remove('on');
+        verifyMaestro14 = false;
+    }
+})
+
+const maestro15 = document.querySelector('.maestro15');
+const dataMaestro15 = document.querySelector('.table-data15');
+let verifyMaestro15 = false;
+
+maestro15.addEventListener('click' , () => {
+    if(!verifyMaestro15) {
+        dataMaestro15.classList.add('on');
+        verifyMaestro15 = true;
+    } else {
+        dataMaestro15.classList.remove('on');
+        verifyMaestro15 = false;
+    }
+})
+
+const maestro16 = document.querySelector('.maestro16');
+const dataMaestro16= document.querySelector('.table-data16');
+let verifyMaestro16 = false;
+
+maestro16.addEventListener('click', () => {
+    if(!verifyMaestro16) {
+        dataMaestro16.classList.add('on');
+        verifyMaestro16 = true;
+    } else {
+        dataMaestro16.classList.remove('on');
+        verifyMaestro16 = false;
+    }
+})
+
+const maestro17 = document.querySelector('.maestro17');
+const dataMaestro17 = document.querySelector('.table-data17');
+let verifyMaestro17 = false;
+
+
+maestro17.addEventListener('click', () => {
+    if(!verifyMaestro17) {
+        dataMaestro17.classList.add('on');
+        verifyMaestro17 = true;
+    } else {
+        dataMaestro17.classList.remove('on')
+        verifyMaestro17 = false;
+    }
+});
+
+const maestro18 = document.querySelector('.maestro18');
+const dataMaestro18 = document.querySelector('.table-data18');
+let verifyMaestro18 = false;
+
+maestro18.addEventListener('click', () => {
+    if(!verifyMaestro18) {
+        dataMaestro18.classList.add('on');
+        verifyMaestro18 = true;
+    } else {
+        dataMaestro18.classList.remove('on');
+        verifyMaestro18 = false;
+    }
+})
+
+const maestro19 = document.querySelector('.maestro19');
+const dataMaestro19 = document.querySelector('.table-data19');
+let verifyMaestro19 = false;
+
+maestro19.addEventListener('click' , () => {
+    if(!verifyMaestro19) {
+        dataMaestro19.classList.add('on');
+        verifyMaestro19 = true;
+    } else {
+        dataMaestro19.classList.remove('on');
+        verifyMaestro19 = false;
+    }
+})
+
+const maestro20 = document.querySelector('.maestro20');
+const dataMaestro20 = document.querySelector('.table-data20');
+let verifyMaestro20 = false;
+
+maestro20.addEventListener('click' , () => {
+    if(!verifyMaestro20) {
+        dataMaestro20.classList.add('on');
+        verifyMaestro20 = true;
+    } else {
+        dataMaestro20.classList.remove('on');
+        verifyMaestro20 = false;
+    }
+})
